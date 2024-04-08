68de2255f73413820476b5d8b28bf9f20bc4986e
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-04-08 19:01:23 +0200

    adding current unversioned files

---
+++ src/Data/CarData.java
@@ -0,0 +1,51 @@
+package Data;
+
+import java.util.ArrayList;
+
+public class CarData {
+
+	private ArrayList<RefuleTank> refuleTanks;
+
+	private ArrayList<RepairJob> repairJobs;
+
+	public CarData() {
+		load();
+	}
+
+	public void printOrderedRefules() {
+
+	}
+
+	public void printTotalPrices() {
+
+	}
+
+	public void printLiterPrices() {
+
+	}
+
+	public void printLiters() {
+
+	}
+
+	public void printLpkm() {
+
+	}
+
+	public void printKilometers() {
+
+	}
+
+	public void printRepairJobs(){
+
+	}
+
+	public void save() {
+
+	}
+
+	public void load() {
+
+	}
+
+}
---
+++ src/comparators/LitersComparator.java
@@ -0,0 +1,14 @@
+package comparators;
+
+import Data.RefuleTank;
+import java.util.Comparator;
+
+public class LitersComparator
+		implements Comparator<RefuleTank>
+{
+
+	public int compare(RefuleTank rt1, RefuleTank rt2) {
+		return 0;
+	}
+
+}
