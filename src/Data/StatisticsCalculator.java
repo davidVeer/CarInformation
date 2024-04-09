d26b3890d9c8125db54b2a4ffa3fdada791d402d
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-04-09 19:20:15 +0200

    removing unnecessary methods

--- src/Data/StatisticsCalculator.java
+++ src/Data/StatisticsCalculator.java
@@ -4,71 +4,6 @@ import java.util.ArrayList;
 
 public class StatisticsCalculator {
 
-    //statistic calculations with doubles
-    private double highestDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-    private double lowestDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-    private double highestDeltaDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-    private double averageDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-    private double medianDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-    private double modeDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-    private double standardDeviationDouble(ArrayList<Double> doubles) {
-        return 0;
-    }
-
-
-    //statistic calculations with integers
-    public int highestInt(ArrayList<Integer> integers) {
-        int highestValue = integers.get(0);
-        for (Integer integer : integers) {
-            if (integer > highestValue)
-                highestValue = integer;
-        }
-        return highestValue;
-
-    }
-
-    public int lowestInt(ArrayList<Integer> integers) {
-        return 0;
-    }
-
-    public int highestDeltaInt(ArrayList<Integer> integers) {
-        return 0;
-    }
-
-    public int averageInt(ArrayList<Integer> integers) {
-        return 0;
-    }
-
-    public int medianInt(ArrayList<Integer> integers) {
-        return 0;
-    }
-
-    public int modeInt(ArrayList<Integer> integers) {
-        return 0;
-    }
-
-    public int standardDeviationInt(ArrayList<Integer> integers) {
-        return 0;
-    }
-
     //statistics returning RefuleTanks
     public RefuleTank highestRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
         RefuleTank highestValue = refuleTanks.get(0);
@@ -99,7 +34,6 @@ public class StatisticsCalculator {
         }
         return highestValue;
     }
-
     public RefuleTank lowestRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
         RefuleTank lowestValue = refuleTanks.get(0);
 
@@ -129,23 +63,15 @@ public class StatisticsCalculator {
         }
         return lowestValue;
     }
-
-    public RefuleTank highestDeltaRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
-        return null;
-    }
-
     public RefuleTank averageRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
         return null;
     }
-
     public RefuleTank medianRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
         return null;
     }
-
     public RefuleTank modeRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
         return null;
     }
-
     public RefuleTank standardDeviationRefuleTank(ArrayList<RefuleTank> refuleTanks, InformationType wantedInformation) {
         return null;
     }
