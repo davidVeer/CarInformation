47405d252a925282fa83b0fded6df6a36460be36
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-07 23:16:48 +0200

    added variants on tests (for better integrity)

--- test/CarInformation/Util/StatisticsCalculatorTest.java
+++ test/CarInformation/Util/StatisticsCalculatorTest.java
@@ -34,7 +34,7 @@ class StatisticsCalculatorTest {
     }
 
     @Test
-    void highestNumbersTest() {
+    void highestNumbersMixedTest() {
         standardSetup();
         int initialSize = testTanks.size();
 
@@ -44,8 +44,34 @@ class StatisticsCalculatorTest {
                 RefuelTank CALCULATED_HIGHEST_VALUE = calculator.highestRefuelTank(testType);
                 assertHighestNumberInValidList(testType, CALCULATED_HIGHEST_VALUE);
             } catch (NullPointerException exception) {
-                System.out.println("all Items within " + testType + " are invalid");
+                System.out.println("highestNumbersMixed : all Items within " + testType + " are invalid");
             }
+        }
+    }
+    @Test
+    void highestNumbersNegativeTest() {
+        standardSetup("negative");
+        int initialSize = testTanks.size();
+
+        for (InformationType testType : testTypes) {
+            try {
+                assertEquals(initialSize, testTanks.size());
+                RefuelTank CALCULATED_HIGHEST_VALUE = calculator.highestRefuelTank(testType);
+                assertHighestNumberInValidList(testType, CALCULATED_HIGHEST_VALUE);
+            } catch (NullPointerException exception) {
+                System.out.println("highestNumbersNegative : all Items within " + testType + " are invalid");
+            }
+        }
+    }
+    @Test
+    void highestNumbersPositiveTest() {
+        standardSetup("positive");
+        int initialSize = testTanks.size();
+
+        for (InformationType testType : testTypes) {
+                assertEquals(initialSize, testTanks.size());
+                RefuelTank CALCULATED_HIGHEST_VALUE = calculator.highestRefuelTank(testType);
+                assertHighestNumberInValidList(testType, CALCULATED_HIGHEST_VALUE);
         }
     }
 
@@ -59,7 +85,7 @@ class StatisticsCalculatorTest {
     }
 
     @Test
-    void lowestNumbersTest() {
+    void lowestNumbersMixedTest() {
         standardSetup();
 
         for (InformationType testType : testTypes) {
@@ -67,8 +93,32 @@ class StatisticsCalculatorTest {
                 RefuelTank CALCULATED_LOWEST_VALUE = calculator.lowestRefuelTank(testType);
                 assertLowestValueInValidList(testType, CALCULATED_LOWEST_VALUE);
             } catch (NullPointerException exception) {
-                System.out.println("all Items within " + testType + " are invalid");
+                System.out.println("lowestNumbersMixed : all Items within " + testType + " are invalid");
             }
+        }
+    }
+
+    @Test
+    void lowestNumbersNegativeTest() {
+        standardSetup("negative");
+
+        for (InformationType testType : testTypes) {
+            try {
+                RefuelTank CALCULATED_LOWEST_VALUE = calculator.lowestRefuelTank(testType);
+                assertLowestValueInValidList(testType, CALCULATED_LOWEST_VALUE);
+            } catch (NullPointerException exception) {
+                System.out.println("lowestNumbersNegative : all Items within " + testType + " are invalid");
+            }
+        }
+    }
+
+    @Test
+    void lowestNumbersPositiveTest() {
+        standardSetup("positive");
+
+        for (InformationType testType : testTypes) {
+                RefuelTank CALCULATED_LOWEST_VALUE = calculator.lowestRefuelTank(testType);
+                assertLowestValueInValidList(testType, CALCULATED_LOWEST_VALUE);
         }
     }
 
