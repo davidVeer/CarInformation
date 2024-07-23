27bb0482121a58badf5ecb322ba95e933449eba0
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-23 16:10:50 +0200

    added the tests for positive Variance

--- test/CarInformation/Util/PositiveValues/PositiveVarianceTest.java
+++ test/CarInformation/Util/PositiveValues/PositiveVarianceTest.java
@@ -1,8 +1,10 @@
 package CarInformation.Util.PositiveValues;
 
+import CarInformation.Util.InformationType;
 import CarInformation.Util.TestFoundations.CalculatorVarianceTest;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class PositiveVarianceTest extends PositiveValues implements CalculatorVarianceTest {
@@ -10,37 +12,69 @@ public class PositiveVarianceTest extends PositiveValues implements CalculatorVa
 	@Override
 	@Test
 	public void refuelNumberVariance() {
-		fail();
+		setup();
+		double CALCULATED_VALUE =
+				(int)(calculator.populationVarianceRefuelTank(InformationType.REFUEL_NUMBER) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 2.0 ;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersVariance() {
-		fail();
+		setup();
+		double CALCULATED_VALUE =
+				(int)(calculator.populationVarianceRefuelTank(InformationType.KILOMETERS_DRIVEN) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 2322.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void litersVariance() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = (int)(calculator.populationVarianceRefuelTank(InformationType.LITERS) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 27.097;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void refuelPriceVariance() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = (int)(calculator.populationVarianceRefuelTank(InformationType.REFUEL_PRICE) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 132.023;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void LiterPriceVariance() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = (int)(calculator.populationVarianceRefuelTank(InformationType.LITER_PRICE) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 0.546;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterVariance() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = (int)(calculator.populationVarianceRefuelTank(InformationType.KILOMETERS_PER_LITER) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 32.629;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+		breakdown();
 	}
 
 }
