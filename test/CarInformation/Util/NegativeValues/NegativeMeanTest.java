238a4d9bfe70f83bd2c5da3bf13b618bb12e77bd
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-23 17:57:26 +0200

    wrote tests for mean Negative Values

--- test/CarInformation/Util/NegativeValues/NegativeMeanTest.java
+++ test/CarInformation/Util/NegativeValues/NegativeMeanTest.java
@@ -1,45 +1,113 @@
 package CarInformation.Util.NegativeValues;
 
+import CarInformation.Data.CarData;
+import CarInformation.Data.RefuelTank;
+import CarInformation.Util.InformationType;
+import CarInformation.Util.StatisticsCalculator;
 import CarInformation.Util.TestFoundations.CalculatorMeanTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NegativeMeanTest extends NegativeValues implements CalculatorMeanTest {
 
 	@Override
 	@Test
 	public void refuelNumberMean() {
-		fail();
+		super.setup();
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.meanRefuelTank(InformationType.REFUEL_NUMBER));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersMean() {
-		fail();
+		super.setup();
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.meanRefuelTank(InformationType.KILOMETERS_DRIVEN));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void litersMean() {
-		fail();
+		super.setup();
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.meanRefuelTank(InformationType.LITERS));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void refuelPriceMean() {
-		fail();
+		super.setup();
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.meanRefuelTank(InformationType.REFUEL_PRICE));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void LiterPriceMean() {
-		fail();
+		setup();
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.meanRefuelTank(InformationType.LITER_PRICE));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterMean() {
-		fail();
+		setup();
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.meanRefuelTank(InformationType.KILOMETERS_PER_LITER));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
+	}
+
+	@Override
+	public void setup() {
+		// Kilometers driven: -1, literPrice :  , kilometers/Liter :
+		super.testTank1 = new RefuelTank(-1, 300, 30,-62.2, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 30 , literPrice :  , kilometers/Liter :
+		super.testTank2 = new RefuelTank(-2, 330, -26.6,43.44, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 170 , literPrice :  , kilometers/Liter :
+		super.testTank3 = new RefuelTank(-3, 500, -91.18,83.32, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 110 , literPrice : , kilometers/Liter :
+		super.testTank4 = new RefuelTank(-4, 610, -0.87,1.2, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 70 , literPrice : , kilometers/Liter :
+		super.testTank5 = new RefuelTank(-5, 780, -6.94,12.0, LocalDate.of(2005, Month.SEPTEMBER,3));
+
+		super.testTanks = new ArrayList<>();
+		testTanks.add(testTank1);
+		testTanks.add(testTank2);
+		testTanks.add(testTank3);
+		testTanks.add(testTank4);
+		testTanks.add(testTank5);
+
+		super.calculator = new StatisticsCalculator(new CarData(testTanks).getRefuelTanks());
+
 	}
 }
