0e7530564c238c80a9899b2713187e33ca2b27ef
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-24 18:41:35 +0200

    setting test for all Identical Lowest values

--- test/CarInformation/Util/IdenticalValues/IdenticalLowest.java
+++ test/CarInformation/Util/IdenticalValues/IdenticalLowest.java
@@ -1,46 +1,114 @@
 package CarInformation.Util.IdenticalValues;
 
+import CarInformation.Data.RefuelTank;
+import CarInformation.Util.InformationType;
 import CarInformation.Util.TestFoundations.CalculatorLowestTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IdenticalLowest extends IdenticalValues implements CalculatorLowestTest {
 
 	@Override
 	@Test
 	public void refuelNumberLowest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.lowestRefuelTank(InformationType.REFUEL_NUMBER);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+		EXPECTED_VALUES.add(testTank5);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersLowest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.lowestRefuelTank(InformationType.KILOMETERS_DRIVEN);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+		EXPECTED_VALUES.add(testTank5);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void litersLowest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.lowestRefuelTank(InformationType.LITERS);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+		EXPECTED_VALUES.add(testTank5);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void refuelPriceLowest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.lowestRefuelTank(InformationType.REFUEL_PRICE);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+		EXPECTED_VALUES.add(testTank5);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void LiterPriceLowest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.lowestRefuelTank(InformationType.LITER_PRICE);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+		EXPECTED_VALUES.add(testTank5);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterLowest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.lowestRefuelTank(InformationType.KILOMETERS_PER_LITER);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+		EXPECTED_VALUES.add(testTank5);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 }
