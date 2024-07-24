e40ea0067e195544fb066242231e0657c0051832
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-24 12:13:33 +0200

    setting test for all Identical highest values

--- test/CarInformation/Util/IdenticalValues/IdenticalHighestTest.java
+++ test/CarInformation/Util/IdenticalValues/IdenticalHighestTest.java
@@ -1,7 +1,11 @@
 package CarInformation.Util.IdenticalValues;
 
+import CarInformation.Data.RefuelTank;
+import CarInformation.Util.InformationType;
 import CarInformation.Util.TestFoundations.CalculatorHighestTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,39 +15,99 @@ public class IdenticalHighestTest extends IdenticalValues implements CalculatorH
 	@Override
 	@Test
 	public void refuelNumberHighest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.highestRefuelTank(InformationType.REFUEL_NUMBER);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank5);
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersHighest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.highestRefuelTank(InformationType.KILOMETERS_DRIVEN);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank5);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void litersHighest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.highestRefuelTank(InformationType.LITERS);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank5);
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 
 	@Override
 	@Test
 	public void refuelPriceHighest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.highestRefuelTank(InformationType.REFUEL_PRICE);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank5);
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void LiterPriceHighest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.highestRefuelTank(InformationType.LITER_PRICE);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank5);
+		EXPECTED_VALUES.add(testTank1);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
+
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterHighest() {
-		fail();
+		super.setup();
+		ArrayList<RefuelTank> CALCULATED_VALUES = calculator.highestRefuelTank(InformationType.KILOMETERS_PER_LITER);
+		ArrayList<RefuelTank> EXPECTED_VALUES = new ArrayList<>();
+
+		EXPECTED_VALUES.add(testTank5);
+		EXPECTED_VALUES.add(testTank2);
+		EXPECTED_VALUES.add(testTank3);
+		EXPECTED_VALUES.add(testTank4);
+
+		assertEquals(EXPECTED_VALUES, CALCULATED_VALUES);
 	}
-
-
 }
