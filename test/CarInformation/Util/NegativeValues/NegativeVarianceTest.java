64b3d005e6806286caba66c80ad3cb9c5d3b6dbb
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-24 11:12:47 +0200

    added tests for Negative Variance

--- test/CarInformation/Util/NegativeValues/NegativeVarianceTest.java
+++ test/CarInformation/Util/NegativeValues/NegativeVarianceTest.java
@@ -1,45 +1,124 @@
 package CarInformation.Util.NegativeValues;
 
+import CarInformation.Data.CarData;
+import CarInformation.Data.RefuelTank;
+import CarInformation.Util.InformationType;
+import CarInformation.Util.StatisticsCalculator;
 import CarInformation.Util.TestFoundations.CalculatorVarianceTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NegativeVarianceTest extends NegativeValues implements CalculatorVarianceTest {
 
 	@Override
 	@Test
 	public void refuelNumberVariance() {
-		fail();
+		super.setup();
+
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.populationVarianceRefuelTank(InformationType.REFUEL_NUMBER));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersVariance() {
-		fail();
+		super.setup();
+
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.populationVarianceRefuelTank(InformationType.KILOMETERS_DRIVEN));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void litersVariance() {
-		fail();
+		super.setup();
+
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.populationVarianceRefuelTank(InformationType.LITERS));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void refuelPriceVariance() {
-		fail();
+		super.setup();
+
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.populationVarianceRefuelTank(InformationType.REFUEL_PRICE));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void LiterPriceVariance() {
-		fail();
+		setup();
+
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.populationVarianceRefuelTank(InformationType.LITER_PRICE));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterVariance() {
-		fail();
+		setup();
+
+		NullPointerException expectedException = assertThrows(NullPointerException.class ,() ->
+				calculator.populationVarianceRefuelTank(InformationType.KILOMETERS_PER_LITER));
+
+		assertEquals("all values were invalid",expectedException.getMessage());
+
+		breakdown();
+
+	}
+
+	@Override
+	public void setup() {
+		// Kilometers driven: -1, literPrice :  , kilometers/Liter :
+		super.testTank1 = new RefuelTank(-1, 300, 30,-62.2, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 30 , literPrice :  , kilometers/Liter :
+		super.testTank2 = new RefuelTank(-2, 330, -26.6,43.44, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 170 , literPrice :  , kilometers/Liter :
+		super.testTank3 = new RefuelTank(-3, 500, -91.18,83.32, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 110 , literPrice : , kilometers/Liter :
+		super.testTank4 = new RefuelTank(-4, 610, -0.87,1.2, LocalDate.of(2005, Month.SEPTEMBER,3));
+		// Kilometers driven: 70 , literPrice : , kilometers/Liter :
+		super.testTank5 = new RefuelTank(-5, 780, -6.94,12.0, LocalDate.of(2005, Month.SEPTEMBER,3));
+
+		super.testTanks = new ArrayList<>();
+		testTanks.add(testTank1);
+		testTanks.add(testTank2);
+		testTanks.add(testTank3);
+		testTanks.add(testTank4);
+		testTanks.add(testTank5);
+
+		super.calculator = new StatisticsCalculator(new CarData(testTanks).getRefuelTanks());
+
 	}
 }
