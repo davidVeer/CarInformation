69b776cd3ffef0853039b5f3f176be2107be7720
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-25 10:46:20 +0200

    setting test for all Identical total values

--- test/CarInformation/Util/IdenticalValues/IdenticalTotalTest.java
+++ test/CarInformation/Util/IdenticalValues/IdenticalTotalTest.java
@@ -1,8 +1,10 @@
 package CarInformation.Util.IdenticalValues;
 
+import CarInformation.Util.InformationType;
 import CarInformation.Util.TestFoundations.CalculatorTotalTest;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class IdenticalTotalTest extends IdenticalValues implements CalculatorTotalTest {
@@ -10,37 +12,78 @@ public class IdenticalTotalTest extends IdenticalValues implements CalculatorTot
 	@Override
 	@Test
 	public void refuelNumberTotal() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.totalValueRefuelTank(InformationType.REFUEL_NUMBER);
+		double EXPECTED_VALUE = Double.NaN;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersTotal() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.totalValueRefuelTank(InformationType.KILOMETERS_DRIVEN);
+		double EXPECTED_VALUE = 80;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void litersTotal() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = (int)(calculator.totalValueRefuelTank(InformationType.LITERS) * 1000) / 1000.0;
+		double EXPECTED_VALUE = 43.449;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void refuelPriceTotal() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = (int)(calculator.totalValueRefuelTank(InformationType.REFUEL_PRICE) * 100) / 100.0;
+		double EXPECTED_VALUE = 91.05;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void LiterPriceTotal() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.totalValueRefuelTank(InformationType.LITER_PRICE);
+		double EXPECTED_VALUE = Double.NaN;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterTotal() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.totalValueRefuelTank(InformationType.KILOMETERS_PER_LITER);
+		double EXPECTED_VALUE = Double.NaN;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 
