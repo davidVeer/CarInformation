5b89383c9b64297d58c9112b8d23dd16a8032b38
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-25 10:37:33 +0200

    setting test for all Identical Standar deviation values

--- test/CarInformation/Util/IdenticalValues/IdenticalStandardDeviationTest.java
+++ test/CarInformation/Util/IdenticalValues/IdenticalStandardDeviationTest.java
@@ -1,47 +1,88 @@
 package CarInformation.Util.IdenticalValues;
 
+import CarInformation.Util.InformationType;
 import CarInformation.Util.TestFoundations.CalculatorStandardDeviation;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IdenticalStandardDeviationTest extends IdenticalValues implements CalculatorStandardDeviation {
 
 	@Override
 	@Test
 	public void refuelNumberStandardDeviation() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.populationStandardDeviationRefuelTank(InformationType.REFUEL_NUMBER);
+		double EXPECTED_VALUE = 0.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersStandardDeviation() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.populationStandardDeviationRefuelTank(InformationType.KILOMETERS_DRIVEN);
+		double EXPECTED_VALUE = 0.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void litersStandardDeviation() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.populationStandardDeviationRefuelTank(InformationType.LITERS);
+		double EXPECTED_VALUE = 0.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void refuelPriceStandardDeviation() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.populationStandardDeviationRefuelTank(InformationType.REFUEL_PRICE);
+		double EXPECTED_VALUE = 0.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void LiterPriceStandardDeviation() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.populationStandardDeviationRefuelTank(InformationType.LITER_PRICE);
+		double EXPECTED_VALUE = 0.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterStandardDeviation() {
-		fail();
+		setup();
+		double CALCULATED_VALUE = calculator.populationStandardDeviationRefuelTank(InformationType.KILOMETERS_PER_LITER);
+		double EXPECTED_VALUE = 0.0;
+
+		assertEquals(EXPECTED_VALUE,CALCULATED_VALUE);
+
+		breakdown();
+
 	}
 
 }
