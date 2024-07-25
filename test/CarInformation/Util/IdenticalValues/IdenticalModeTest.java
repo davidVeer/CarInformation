ec97fa67cc57aa61098990110bfb5f54a558be91
davidVeer <151018675+davidVeer@users.noreply.github.com>
2024-07-25 10:28:21 +0200

    setting test for all Identical mode values

--- test/CarInformation/Util/IdenticalValues/IdenticalModeTest.java
+++ test/CarInformation/Util/IdenticalValues/IdenticalModeTest.java
@@ -1,46 +1,89 @@
 package CarInformation.Util.IdenticalValues;
 
+import CarInformation.Util.InformationType;
 import CarInformation.Util.TestFoundations.CalculatorModeTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IdenticalModeTest extends IdenticalValues implements CalculatorModeTest {
 
 	@Override
 	@Test
 	public void refuelNumberMode() {
-		fail();
+		setup();
+		ArrayList<Double> CALUCATED_VALUE = calculator.modeRefuelTank(InformationType.REFUEL_NUMBER);
+		ArrayList<Double> EXPECTED_VALUE = new ArrayList<>();
+
+		EXPECTED_VALUE.add(1.0);
+
+		assertEquals(EXPECTED_VALUE, CALUCATED_VALUE);
 	}
 
 	@Override
 	@Test
 	public void drivenKilometersMode() {
-		fail();
+		setup();
+		ArrayList<Double> CALUCATED_VALUE = calculator.modeRefuelTank(InformationType.KILOMETERS_DRIVEN);
+		ArrayList<Double> EXPECTED_VALUE = new ArrayList<>();
+
+		EXPECTED_VALUE.add(20.0);
+
+		assertEquals(EXPECTED_VALUE, CALUCATED_VALUE);
+
 	}
 
 	@Override
 	@Test
 	public void litersMode() {
-		fail();
+		setup();
+		ArrayList<Double> CALUCATED_VALUE = calculator.modeRefuelTank(InformationType.LITERS);
+		ArrayList<Double> EXPECTED_VALUE = new ArrayList<>();
+
+		EXPECTED_VALUE.add(8.0);
+
+		assertEquals(EXPECTED_VALUE, CALUCATED_VALUE);
+
 	}
 
 	@Override
 	@Test
 	public void refuelPriceMode() {
-		fail();
+		setup();
+		ArrayList<Double> CALUCATED_VALUE = calculator.modeRefuelTank(InformationType.REFUEL_PRICE);
+		ArrayList<Double> EXPECTED_VALUE = new ArrayList<>();
+
+		EXPECTED_VALUE.add(18.0);
+
+		assertEquals(EXPECTED_VALUE, CALUCATED_VALUE);
+
 	}
 
 	@Override
 	@Test
 	public void LiterPriceMode() {
-		fail();
+		setup();
+		ArrayList<Double> CALUCATED_VALUE = calculator.modeRefuelTank(InformationType.LITER_PRICE);
+		ArrayList<Double> EXPECTED_VALUE = new ArrayList<>();
+
+		EXPECTED_VALUE.add(2.0);
+
+		assertEquals(EXPECTED_VALUE, CALUCATED_VALUE);
+
 	}
 
 	@Override
 	@Test
 	public void kilometersPerLiterMode() {
-		fail();
+		setup();
+		ArrayList<Double> CALUCATED_VALUE = calculator.modeRefuelTank(InformationType.KILOMETERS_PER_LITER);
+		ArrayList<Double> EXPECTED_VALUE = new ArrayList<>();
+
+		EXPECTED_VALUE.add(2.0);
+
+		assertEquals(EXPECTED_VALUE, CALUCATED_VALUE);
 	}
 
 
