cd7dd08733d058f428724ec2616bd8615b125a96
davidVeer <davidvdveer05@gmail.com>
2025-10-08 13:53:04 +0200

    Generating skeleton functions for statisticsCalculator

--- CarStatsServer/Calculators/StatisticsCalculator.cs
+++ CarStatsServer/Calculators/StatisticsCalculator.cs
@@ -1,12 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace CarStatsServer.Calculators
 {
     public class StatisticsCalculator
     {
+        public static float CalculateMode(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+        public static float CalculateMean(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+        public static float CalculateStandardDeviation(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+        public static float CalculateIqr(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+        public static float FindMedian(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+        public static float FindHighest(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+        public static float FindLowest(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Hashtable MakeFrequencyTable(float[] dataListFloats)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static float CalculateTotal(float[] dataListFloats) 
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public static uint CalculateMode(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static uint CalculateMean(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static uint CalculateStandardDeviation(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static uint CalculateIqr(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static uint FindMedian(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static uint FindHighest(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static uint FindLowest(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+        public static Hashtable MakeFrequencyTable(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static uint CalculateTotal(uint[] dataListInts)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
