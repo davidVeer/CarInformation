66122270672d40a13a1060e5b0d3b2fdebedb4f0
davidVeer <davidvdveer05@gmail.com>
2025-10-08 13:29:21 +0200

    adding empty functions according to model (DataCalculator)

--- CarStatsServer/Calculators/DataCalculator.cs
+++ CarStatsServer/Calculators/DataCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarStatsServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace CarStatsServer.Calculators
 {
     public class DataCalculator
     {
+        public ushort CalculateKilometerDriven(uint oldOdometerReading, uint newOdometerReading)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float CalculateFuelConsumption(ushort kilometersDriven, float litersUsed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float CalculatePricePerLiter(float litersUsed, float fuelUpCost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float CalculateEmissions(ushort kilometersDriven, float fuelConsumption, FuelType fuelType )
+        {
+            throw new NotImplementedException();
+        }      
     }
 }
