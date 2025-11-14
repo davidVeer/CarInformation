b6fc63dcb1f5fda1510438d077dd3fafee467cba
davidVeer <davidvdveer05@gmail.com>
2025-11-14 11:56:10 +0100

    Getting Program ready to merge with development
    the Database function for changing fuel up data is not all that useful for now, deleting and reentering an entry can serve as a poor-man's update for now. Because of this I'd like to call this feature ready for the development branch so I can work on calculations which is the central focus of this project.

--- CarStatsServer/Program.cs
+++ CarStatsServer/Program.cs
@@ -3,8 +3,6 @@ namespace CarStatsServer
     public class Program
     {
         static void Main() {
-            TempDebugFunctions.PrintFuelUpList();
-            TempDebugFunctions.EnterNewFuelUp();
         }
 
     }
